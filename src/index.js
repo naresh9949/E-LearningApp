979d940f070e52901a9b353e1fd302d6b58b0f1f
naresh9949 <nareshkollipora9949@gmail.com>
2022-01-06 17:06:53 +0530

    added ssl

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import HttpsRedirect from "react-https-redirect";
+import App from "./App";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <HttpsRedirect>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </HttpsRedirect>,
+  document.getElementById("root")
 );
